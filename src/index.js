a0c37066d77f716ec63805f79daae722a558dc10
Rodrigo Lima <rodrigoalves1901@hotmail.com>
2026-05-01 23:04:26 -0300

    Fix: Add missing path import for production build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 import { searchAggregatedJobs } from './services/jobScraperService.js';
 import { 
   listAutomations, 
